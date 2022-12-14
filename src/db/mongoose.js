f2368f817eb59d306a43c1a5c14b8607bde604ab
anthonynwaweru <anthonypmarcel@gmail.com>
2022-12-14 11:53:48 +0300

    mongoose update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,7 +5,6 @@ mongoose
   .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   })
   .then(() => console.log("Database connected!"))
   .catch((err) => console.log(err));
