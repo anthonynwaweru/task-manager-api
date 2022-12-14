19a026c39942e72d4c35f4211a12d72f49d1dcf4
anthonynwaweru <anthonypmarcel@gmail.com>
2022-12-14 11:40:15 +0300

    mongoose update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options. Mongoose 6 always behaves as if useNew
-mongoose.connect(`${process.env.MONGODB_URL}`, {});
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
